808819c67ca9d452a037f3abd74a7c0ba045441c
fr6nck <fr6nck@me.org>
2022-04-26 22:32:22 +0200

    Working on Race DAO

--- scripts/load_data_to_model.py
+++ scripts/load_data_to_model.py
@@ -1,16 +1,28 @@
-from sqlalchemy import create_engine
 from dotenv import load_dotenv
-from sqlalchemy.orm.session import sessionmaker
-from source.model import Base
 
+from source.dao import RaceDao
+from source.model import Race
+from source.utils import set_logger, get_mongo_data_service
+import logging
+import os
 
 set_logger()
 
 load_dotenv()
 
-DB_URI = os.environ['MODEL_DB_URI']
-engine = create_engine(DB_URI)
+data_service = get_mongo_data_service()
 
-Base.metadata.create_all(engine) # here we create all tables
-Session = sessionmaker(bind=engine)
-session = Session()
+DB_URI = os.environ['MODEL_DB_URI']
+programs = data_service.get_all_programs()
+race_dao = RaceDao(DB_URI)
+
+for program in programs:
+    for meeting in program['reunions']:
+        for race in meeting['courses']:
+            raceModel = Race.fromJson(race, meeting)
+            race_dao.save_race(raceModel)
+
+
+races = race_dao.get_race_by_identity('asdf', 2, 3)
+for race in races:
+    logging.info(race.racetrack_name)
--- source/dao.py
+++ source/dao.py
@@ -1,20 +1,38 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm.session import sessionmaker
-from source.model import Base, Horse
+from source.model import Base, Horse, Race
 from sqlalchemy import select
 
 
-class HorseDao():
-    __init__(self, db_uri):
-        engine = create_engine(DB_URI)
-        Base.metadata.create_all(engine) # here we create all tables
-        Session = sessionmaker(bind=engine)
-        self.session = Session()
+class HorseDao:
+    def __init__(self, db_uri):
+        engine = create_engine(db_uri)
+        Base.metadata.create_all(engine)
+        session = sessionmaker(bind=engine)
+        self.session = session()
 
-    save_horse(self, horse):
+    def save_horse(self, horse):
         self.session.add(horse)
 
-    get_horse_by_name(name):
+    def get_horse_by_name(self, name):
         statement = select(Horse).where(Horse.name == name)
-        horse = self.session.execute(statement).
+        horse = self.session.execute(statement)
         return horse
+
+
+class RaceDao:
+    def __init__(self, db_uri):
+        engine = create_engine(db_uri)
+        Base.metadata.create_all(engine)
+        session = sessionmaker(bind=engine)
+        self.session = session()
+
+    def save_race(self, race):
+        self.session.add(race)
+
+    def get_race_by_identity(self, date, meeting_id, race_id):
+        """Identity is aggregation of date, meeting_id and race_id"""
+        statement = select(Race).where(Race.meeting_id == meeting_id)
+        race = self.session.execute(statement).scalars()
+        return race
+
