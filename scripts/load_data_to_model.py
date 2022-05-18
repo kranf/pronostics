a232c0daa323b5ef44ecd037d3039811ff7fd952
fr6nck <fr6nck@me.org>
2022-05-18 21:46:04 +0200

    WIP: data service to persist model
    session factory

--- scripts/load_data_to_model.py
+++ scripts/load_data_to_model.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+
 from dotenv import load_dotenv
 
-from source.dao import RaceDao
+from source import settings
+from source.dao import RaceDao, create_session
 from source.data_scrapping import get_mongo_data_service
 from source.model import Race
 from source.utils import set_logger
@@ -14,14 +17,21 @@ load_dotenv()
 data_service = get_mongo_data_service()
 
 DB_URI = os.environ['MODEL_DB_URI']
-races = data_service.get_races_by_date('29032022')
-race_dao = RaceDao(DB_URI)
+# races = data_service.get_races_by_date('29032022')
 
 # for meeting in races['reunions']:
 #     for race in meeting['courses']:
 #         raceModel = Race.fromJson(race, meeting)
 #         race_dao.save_race(raceModel)
 
+session = create_session(DB_URI)
 
+race_dao = RaceDao(session)
+
+races = race_dao.get_race_by_identity(datetime.strptime('29032022', settings.DATE_FORMAT), 1, 1)
+# logging.info(race[0].name)
+# with session.begin():
 for race in races:
-    logging.info(race.racetrack_name)
+    logging.info(race.name)
+#         race_dao.save_race(race)
+#         session.commit()`
--- source/dao.py
+++ source/dao.py
@@ -1,15 +1,18 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm.session import sessionmaker
+from sqlalchemy.orm import Session
 from source.model import Base, Horse, Race
 from sqlalchemy import select
 
 
+def create_session(db_uri):
+    engine = create_engine(db_uri)
+    Base.metadata.create_all(engine)
+    return Session(engine)
+
+
 class HorseDao:
-    def __init__(self, db_uri):
-        engine = create_engine(db_uri)
-        Base.metadata.create_all(engine)
-        session = sessionmaker(bind=engine)
-        self.session = session()
+    def __init__(self, session):
+        self.session = session
 
     def save_horse(self, horse):
         self.session.add(horse)
@@ -21,18 +24,14 @@ class HorseDao:
 
 
 class RaceDao:
-    def __init__(self, db_uri):
-        engine = create_engine(db_uri)
-        Base.metadata.create_all(engine)
-        session = sessionmaker(bind=engine)
-        self.session = session()
+    def __init__(self, session):
+        self.session = session
 
     def save_race(self, race):
         self.session.add(race)
 
     def get_race_by_identity(self, date, meeting_id, race_id):
         """Identity is aggregation of date, meeting_id and race_id"""
-        statement = select(Race).where(Race.meeting_id == meeting_id)
+        statement = select(Race).where(Race.start_date == date)
         race = self.session.execute(statement).scalars()
         return race
-
