d034913caa17b39c4d074f6b5530337e937fc466
kranf <kranf@users.noreply.github.com>
2022-09-15 22:46:57 +0200

    WIP. FeatureBuilder.

--- source/machine_learning/model.py
+++ source/machine_learning/model.py
@@ -1,4 +1,6 @@
-from source.model import Race
+from typing import TypedDict
+
+from source.model import Race, Participant
 
 
 class Season:
@@ -6,6 +8,29 @@ class Season:
     SPRING = 1
     SUMMER = 2
     AUTUMN = 3
+
+class ParticipantDetailsFeatures(TypedDict):
+    driver_change: bool
+    disadvantage_value: int
+    disadvantage_length: int
+    disadvantage_weight: int
+    disadvantage_value_mean: int
+    disadvantage_length_mean: int
+    disadvantage_weight_mean: int
+    average_speed: float
+
+
+class FeatureBuilder:
+    def __init__(self):
+        pass
+
+    def build_for_race(self, race: Race):
+
+        participantFeaturesList = [ self.build_participant_features(participant) for participant in race.participants ]
+
+    def build_participant_features(self, participant: Participant):
+        pass
+
 
 def to_features(race, participant):
     race_details = {
@@ -25,5 +50,7 @@ def to_features(race, participant):
         'disadvantage_value_mean': participant.disadvantage_value,
         'disadvantage_length_mean': participant.disadvantage_length,
         'disadvantage_weight_mean': participant.disadvantage_weight,
-
+        'average_speed': par
     }
+
+
