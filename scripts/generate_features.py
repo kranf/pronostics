400dc083e160d429d03289eb5911fb3876e48ee9
kranf <kranf@users.noreply.github.com>
2024-01-17 23:19:33 +0100

    Average participant speed feature.
    Use a feature generator class with reference to dataService in order to retrieve horse races

--- scripts/generate_features.py
+++ scripts/generate_features.py
@@ -10,6 +10,7 @@ import pandas as pd
 
 from source.data_service import get_data_service
 from source.machine_learning.mapper import to_features, get_feature_list
+from source.machine_learning.training_dataset_generator import TrainingDatasetGenerator
 from source.utils import set_logger
 
 set_logger()
@@ -20,11 +21,12 @@ DB_URI = os.environ['MODEL_DB_URI']
 TRAINING_DATASET_FILE = 'training-dataset.csv'
 
 dataService = get_data_service(DB_URI)
+generator = TrainingDatasetGenerator(dataService)
 
 races = dataService.get_all_races()
 
 race_series = pd.Series()
 for race in races:
-    features = pd.Series(to_features(race))
+    features = pd.Series(generator.to_features(race))
     race_series = pd.concat([race_series, features])
     print(features)
--- source/machine_learning/training_dataset_generator.py
+++ source/machine_learning/training_dataset_generator.py
@@ -1,4 +1,6 @@
 import logging
+from functools import reduce
+from operator import add
 
 import pandas as pd
 from pandas import concat
@@ -6,29 +8,54 @@ from pandas import concat
 
 class TrainingDatasetGenerator:
 
-    def generate_training_dataset(self, races):
-        features = []
-        for race in races:
-            feature = self.to_race_feature(race)
-            for participant in race.participants:
-                feature.update(self.to_participant_feature(participant))
-            features += [feature]
-        race_data = pd.DataFrame(features)
-        logging.info(race_data)
+    def __init__(self, data_service):
+        self.data_service = data_service
 
-    def to_race_feature(self, race):
+    def to_features(self, race):
+        feature = self.to_race_features(race)
+        for participant in race.participants:
+            feature.update(self.to_participant_features(participant))
+        return feature
+
+    def to_race_features(self, race):
         self.validate_race(race)
         return {
             'is_diurnal': int(race.nature.lower() == 'diurne'),
             'is_nocturnal': int(race.nature.lower() == 'nocturne'),
             'is_half_nocturnal': int(race.nature.lower() == 'seminocturne'),
             'length_in_meter': race.length,
+            'field_is_flat': int(race.field.lower() == 'plat'),
+            'field_is_harness': int(race.field.lower() == 'attele'),
+            'field_is_hurdle': int(race.field.lower() == 'haie'),
+            'field_is_saddle': int(race.field.lower() == 'monte'),
+            'field_is_steeplechase': int(race.field.lower() == 'steeplechase'),
+            'field_is_cross': int(race.field.lower() == 'cross'),
         }
 
-    def to_participant_feature(self, participant):
+    def to_participants_features(self, race):
+        return [self.to_participant_features(participant) for participant in race.participants]
+
+    def to_participant_features(self, participant):
         return {
             f'p{participant.pmu_id}_driver_change': int(participant.driver_change),
             f'p{participant.pmu_id}_lane_id': int(participant.lane_id),
+            f'p{participant.pmu_id}_speed_average': int(self.calculate_average_speed(participant)),
         }
+
     def validate_race(self, race):
         assert race.nature.lower() in ['diurne', 'nocturne', 'seminocturne']
+        assert race.field.lower() in ['plat', 'attele', 'haie', 'monte', 'steeplechase', 'cross']
+
+    def calculate_average_speed(self, participant):
+        participations = self.data_service.get_participations_for_horse(participant.horse.name)
+        return reduce(add, [p.speed for p in participations])/len(participations)
+
+    # def generate_training_dataset(self, races):
+    #     features = []
+    #     for race in races:
+    #         feature = self.to_race_feature(race)
+    #         for participant in race.participants:
+    #             feature.update(self.to_participant_feature(participant))
+    #         features += [feature]
+    #     race_data = pd.DataFrame(features)
+    #     logging.info(race_data)
